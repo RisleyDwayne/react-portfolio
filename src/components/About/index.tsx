649ae060052df3e3746bb20143ec110976f24442
Dwayne Risley <RisleyDwayne@gmail.com>
2020-11-11 20:48:34 -0500

    add footer, resume

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
 import headshot from '../../assets/images/headshot.jpg';
 
 
@@ -14,6 +15,13 @@ const useStyles = makeStyles((theme: Theme) =>
             backgroundColor: "#f0dde1",
             margin: theme.spacing(3),
             padding: theme.spacing(3),
+            container: {
+                paddingBottom: theme.spacing(4),
+    
+            },
+        },
+        container: {
+            paddingBottom: theme.spacing(4),
         },
         img: {
             width: "100%",
@@ -28,6 +36,19 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         textBlock: {
             margin: theme.spacing(1)
+        },
+        contact: {
+            paddingLeft: theme.spacing(1),
+            margin: 0
+        },
+        icons: {
+            fontSize: "4rem",
+        },
+        iconGroup: {
+            textAlign: "center"
+        },
+        email: {
+            color: theme.palette.text.secondary,
         }
 
     })
@@ -39,7 +60,7 @@ export default function About() {
 
     return (
         <section id="about" className={classes.root}>
-            <Container>
+            <Container className={classes.container}>
                 <Typography component="h2" variant="h3" className={classes.heading}>About Me</Typography>
                 <hr />
                 <Grid container spacing={2}>
@@ -47,8 +68,22 @@ export default function About() {
                         <Paper className={classes.paper}>
 
                             <img src={headshot}
-                                className={classes.img} alt="image of Dwayne Risley" />
+                                className={classes.img}     alt="Dwayne Risley" />
                         </Paper>
+
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography variant="h5" component="h3" className={classes.heading}>
+                                    Contact me:
+                                </Typography>
+                                <Typography className={classes.contact} variant="body1" paragraph >
+                                    <small>Phone: (603) 318-6527</small>
+                                </Typography>
+                                <Typography className={classes.contact} variant="body1" paragraph >
+                                    <small>Email: <a className={classes.email} href="mailto:risleydwayne@gmail.com">RisleyDwayne@gmail.com</a></small>
+                                </Typography>
+                            </Grid>
+                        </Grid>
 
                     </Grid>
                     <Grid item xs={12} sm={8} >
@@ -57,6 +92,27 @@ export default function About() {
                                 I am a Full-Stack Web Developer with experience in Object-Oriented programming, Game Design, and all manner of web technologies including Javascript and frameworks such as React.
                         </Typography>
                         </div>
+
+                        <Grid container spacing={2} className={classes.iconGroup}>
+                            <Grid item xs={4}>
+                                {}
+                                <IconButton color="primary" href="https://github.com/RisleyDwayne" title="GitHub" target="_blank" >
+                                    <i className={`fab fa-github-square ${classes.icons}`}></i>
+                                </IconButton>
+                            </Grid>
+                            <Grid item xs={4} >
+                                <IconButton color="primary" href="https://www.linkedin.com/in/dwayne-risley/" title="LinkedIn" target="_blank">
+                                    <i className={`fab fa-linkedin ${classes.icons}`}></i>
+                                </IconButton>
+
+                            </Grid>
+                            <Grid item xs={4} >
+                                <IconButton color="primary" href="./resume/resume.pdf" title="Resume" target="_blank" download>
+                                    <i className={`fas fa-sticky-note ${classes.icons}`}></i>
+                                </IconButton>
+
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container >
